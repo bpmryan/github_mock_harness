d06a0ef3011c390d65da806a499af2e55212a559
bpmryan <bpm_ryand2428@yahoo.com>
2026-06-11 20:24:09 -0500

    Add api get request

--- code/github_client.py
+++ code/github_client.py
@@ -2,3 +2,15 @@ import requests
 
 def get_public_repo_count(username: str) -> int:
     url = f"https://api.github.com/users/{username}"
+
+    # get request to the target url
+    response = requests.get(url)
+    print(response.json())
+
+    # check for failure 
+    if response.status_code != 200:
+        raise ValueError("GitHub identity lookup failed")
+    
+    # parse the reponse body as JSON and extract the repo count
+    payload = response.json()
+    return payload["public_repos"] # grab integer count of public repo
