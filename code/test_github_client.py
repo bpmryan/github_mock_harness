038301bd6ca074f5d049e9fe91bc9e5683a0b492
bpmryan <bpm_ryand2428@yahoo.com>
2026-06-11 20:30:38 -0500

    Create file to write py tests

---
+++ code/test_github_client.py
@@ -0,0 +1,23 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from github_client import get_public_repo_count
+
+
+@patch("github_client.requests.get")
+def test_get_public_repo_count_success(mock_get):
+    # create a fake response object
+    fake_response = MagicMock()
+    fake_response.status_code = 200
+
+    # tell fake response what its fake JSON dictionary looks like
+    fake_response.json.return_value = {"public_repos": 14}
+
+
+    # intercept requests.get and hand back the fake response instead
+    mock_get.return_value = fake_response
+
+    # execute the final code 
+    result = get_public_repo_count("someuser")
+
+    # assertions
+    
